e3c68b726a0a25f819a43f86d15d0460b1a343ca
Namit Malasi <namitmalasi@gmail.com>
2025-09-28 12:22:44 +0530

    adding budget routes and controllers

--- backend/routes/expenses.js
+++ backend/routes/expenses.js
@@ -4,7 +4,7 @@ import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
-/ @desc    Get all expenses for user
+//@desc    Get all expenses for user
 // @route   GET /api/expenses
 // @access  Private
 router.get('/', auth, async (req, res) => {
