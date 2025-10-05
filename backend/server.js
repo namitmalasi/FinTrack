8e221aff54b6bc20a007f5c78120e38e753c9856
Namit Malasi <namitmalasi@gmail.com>
2025-10-05 11:21:25 +0530

    minor change

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Handle preflight requests for all routes
-app.options("*", cors(corsOptions));
+app.options(/(.*)/, cors(corsOptions));
 
 app.use(express.json());
 
